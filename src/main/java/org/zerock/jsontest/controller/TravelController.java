51c6205c19050fd0ccecd59d1cfcd4fc8edfdc19
LeeHSeoK <gustjr7532@naver.com>
2024-06-11 19:22:28 +0900

    키값 제거

--- src/main/java/org/zerock/jsontest/controller/TravelController.java
+++ src/main/java/org/zerock/jsontest/controller/TravelController.java
@@ -1,5 +1,7 @@
 package org.zerock.jsontest.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
