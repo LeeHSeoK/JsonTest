2e0b6406c3c5a6722090b3764fb7e1cad5e74b14
commang <155868342+ILI-cross@users.noreply.github.com>
2024-06-14 11:55:56 +0900

    이미지 구현 할려하는데 이제 공백은 최소화햇는데 보내주는게 안됨

---
+++ src/main/java/org/zerock/jsontest/dto/ImgDTO.java
@@ -0,0 +1,11 @@
+package org.zerock.jsontest.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ImgDTO {
+    private String firstimg;
+    private String title;
+}
