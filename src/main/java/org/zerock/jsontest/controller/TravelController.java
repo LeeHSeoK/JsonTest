2384dba92be6cca2d8aae6bc88378bb1fded1f16
LeeHSeoK <gustjr7532@naver.com>
2024-06-17 18:12:32 +0900

    일휘씨 home controller layout footer header 등 추가

--- src/main/java/org/zerock/jsontest/controller/TravelController.java
+++ src/main/java/org/zerock/jsontest/controller/TravelController.java
@@ -1,5 +1,6 @@
 package org.zerock.jsontest.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,6 +16,9 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 
 @Controller
@@ -28,12 +32,13 @@ public class TravelController {
 
     SearchListDTO response1;
     ReadOneDTO response2;
-
-    @GetMapping("/search")
-    public String searchKeyword(@RequestParam(value = "keyword", required = false, defaultValue = "대구") String keyword,
-                                @RequestParam(value = "page", required = false, defaultValue = "1") int page,
-                                @RequestParam(value = "numOfRows", required = false, defaultValue = "10") int numOfRows,
-                                Model model) {
+    Random random = new Random();
+    @GetMapping("/home")
+    public String home(@RequestParam(value = "keyword", required = false, defaultValue = "대구") String keyword,
+//                                @RequestParam(value = "page", required = false, defaultValue = "") int page,
+                       @RequestParam(value = "numOfRows", required = false, defaultValue = "20") int numOfRows,
+                       Model model, HttpServletRequest req) {
+        int page = random.nextInt(1)+1;
         try {
             String encodedServiceKey = URLEncoder.encode(SERVICE_KEY, "UTF-8");
             String encodedKeyword = URLEncoder.encode(keyword, "UTF-8");
@@ -46,7 +51,33 @@ public class TravelController {
 
             response1 = new ObjectMapper().readValue(jsonResult, SearchListDTO.class);
 
-            model.addAttribute("items", response1.getResponse().getBody().getItems().getItem());
+            //검증 데이터 추가 06-14
+            // 리스트 받아서 뿌려주고 ?
+            SearchListDTO.Response.Body.Items.Item[] items = response1.getResponse().getBody().getItems().getItem();
+            List<String> imglist = new ArrayList<>();
+            List<String> contentlist = new ArrayList<>();
+            System.out.println("이거실행?");
+            for(int i=0; i<items.length;i++){
+                String img = items[i].getFirstimage();
+                String contentid = items[i].getContentid();
+                if(img.isEmpty()){
+                    continue;
+                }
+
+                imglist.add(img);
+                contentlist.add(contentid);
+            }
+
+            if (imglist.isEmpty() || contentlist.isEmpty()) {
+                // 만약 필요한 데이터가 null이거나 비어있다면 다시 데이터를 받아옴
+                return "redirect:/home?keyword=" + URLEncoder.encode(keyword, "UTF-8") + "&numOfRows=" + numOfRows;
+            }
+
+//            model.addAttribute("items", response1.getResponse().getBody().getItems().getItem());
+            //이미지 리스트 추가'
+            model.addAttribute("img",imglist);
+            model.addAttribute("content", contentlist);
+            req.setAttribute("notice", imglist);
             model.addAttribute("keyword", keyword);
             model.addAttribute("currentPage", page);
             model.addAttribute("numOfRows", numOfRows);
@@ -61,7 +92,69 @@ public class TravelController {
         } catch (Exception e) {
             model.addAttribute("error", "An unexpected error occurred: " + e.getMessage());
         }
-        return "searchresult";
+        return "home";
+    }
+
+    @GetMapping("/search")
+    public String searchKeyword(@RequestParam(value = "keyword", required = false, defaultValue = "대구") String keyword,
+//                                @RequestParam(value = "page", required = false, defaultValue = "") int page,
+                                @RequestParam(value = "numOfRows", required = false, defaultValue = "20") int numOfRows,
+                                Model model, HttpServletRequest req) throws UnsupportedEncodingException {
+        int page = random.nextInt(1)+1;
+        try {
+            String encodedServiceKey = URLEncoder.encode(SERVICE_KEY, "UTF-8");
+            String encodedKeyword = URLEncoder.encode(keyword, "UTF-8");
+            String uri = "http://apis.data.go.kr/B551011/KorService1/searchKeyword1?serviceKey=" + encodedServiceKey +
+                    "&numOfRows=" + numOfRows + "&pageNo=" + page + "&MobileOS=ETC&MobileApp=AppTest&_type=json&listYN=Y&arrange=A&keyword=" + encodedKeyword + "&contentTypeId=12";
+
+            RestTemplate restTemplate = new RestTemplate();
+            URI url = new URI(uri);
+            String jsonResult = restTemplate.getForObject(url, String.class);
+
+            response1 = new ObjectMapper().readValue(jsonResult, SearchListDTO.class);
+
+            //검증 데이터 추가 06-14
+            // 리스트 받아서 뿌려주고 ?
+            SearchListDTO.Response.Body.Items.Item[] items = response1.getResponse().getBody().getItems().getItem();
+            List<String> imglist = new ArrayList<>();
+            List<String> contentlist = new ArrayList<>();
+            List<String> contenttypeidlist = new ArrayList<>();
+            System.out.println("이거실행?");
+            for(int i=0; i<items.length;i++){
+                String img = items[i].getFirstimage();
+                String contentid = items[i].getContentid();
+//수정
+//                String contenttypeid = items[i].getContenttypeid();
+                if(img.isEmpty()){
+                    continue;
+                }
+//수정
+//                contenttypeidlist.add(contenttypeid);
+                imglist.add(img);
+                contentlist.add(contentid);
+            }
+//수정 DTO로 바꿔주세요
+            model.addAttribute("img",imglist);
+            model.addAttribute("content", contentlist);
+            model.addAttribute("notice", imglist);
+            model.addAttribute("keyword", keyword);
+            model.addAttribute("currentPage", page);
+            model.addAttribute("numOfRows", numOfRows);
+//수정
+//            model.addAttribute("contentTypeId",contenttypeidlist);
+            model.addAttribute("totalCount", response1.getResponse().getBody().getTotalCount());
+
+        } catch (HttpClientErrorException e) {
+            model.addAttribute("error", "API key is invalid or not registered.");
+        } catch (URISyntaxException e) {
+            model.addAttribute("error", "URI Syntax Exception: " + e.getMessage());
+        } catch (UnsupportedEncodingException e) {
+            model.addAttribute("error", "Encoding Exception: " + e.getMessage());
+        } catch (Exception e) {
+            model.addAttribute("error", "An unexpected error occurred: " + e.getMessage());
+            return "redirect:/home?keyword=" + URLEncoder.encode(keyword, "UTF-8") + "&numOfRows=" + numOfRows;
+        }
+        return "home";
     }
 
     @GetMapping("/searchresultinfo")
